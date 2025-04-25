997f2e3df2d582fbbc276d9fb8c2ac813e331858
kamangir <arash@kamangir.net>
2025-04-24 23:46:17 -0700

    ++ - kamangir/bolt#746

--- bluer_journal/add/functions.py
+++ bluer_journal/add/functions.py
@@ -1,3 +1,6 @@
+import re
+import calendar
+
 from blueness import module
 from bluer_options import string
 
@@ -20,6 +23,42 @@ def add_message(
             include_time=False,
             as_filename=True,
         )
+
+    if bool(re.fullmatch(r"\d{4}-\d{2}-\d{2}", title)):
+        if verbose:
+            logger.info("date found.")
+
+        title_pieces = [int(piece) for piece in title.split("-")]
+        year = title_pieces[0]
+        month = title_pieces[0]
+
+        if verbose:
+            logger.info(f"adding {year}...")
+        year_page = JournalPage(
+            title=str(year),
+            verbose=verbose,
+        )
+        for month in range(1, 13):
+            if verbose:
+                logger.info(f"adding {year}-{month:02d}...")
+            message_ = f"- [[{year}-{month:02d}]]"
+            if message_ not in year_page.content:
+                year_page.content += [message_]
+
+            month_page = JournalPage(
+                title=f"{year}-{month:02d}",
+                verbose=verbose,
+            )
+            _, last_day = calendar.monthrange(year, month)
+            for day in range(1, last_day + 1):
+                message_ = f"- [[{year}-{month:02d}-{day:02d}]]"
+                if message_ not in month_page.content:
+                    month_page.content += [message_]
+            if not month_page.save(verbose=verbose):
+                return False
+
+        if not year_page.save(verbose=verbose):
+            return False
 
     logger.info(
         "{}.add_message({}{}) -> {}".format(
