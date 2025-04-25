69b9b2e9cc4f188d2b68e4daa2ccd74328c44b57
kamangir <arash@kamangir.net>
2025-04-25 11:45:16 -0700

    pipeline fix, hopefully - kamangir/bolt#746

---
+++ bluer_journal/.abcli/tests/git_check.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+function test_bluer_journal_git_check() {
+    local options=$1
+
+    bluer_journal_git_check
+}
--- bluer_journal/__init__.py
+++ bluer_journal/__init__.py
@@ -4,7 +4,7 @@ ICON = "📜"
 
 DESCRIPTION = f"{ICON} A journal for the age of AI."
 
-VERSION = "5.19.1"
+VERSION = "5.20.1"
 
 REPO_NAME = "bluer-journal"
 
